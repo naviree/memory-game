d9b445b8bb6bfd21dd8ae799d032b7931dd60b23
Jeremy Jefferson <jeremyijefferson@gmail.com>
2023-10-18 12:48:07 -0700

    javascript code

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ const colorsPickList = [...colors, ...colors];
 const tileCount = colorsPickList.length;
 
 let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false;
+
+function buildTile(color) {
+    const element = document.createElement("div");
+
+    element.classList.add("tile");
+}
+
+// Build up tiles
+
+for(let i = 0; i < tileCount; i++){
+    const randomIndex = Math.floor(Math.random () * colorsPickList.length);
+    const color = colorsPickList[randomIndex];
+
+    colorsPickList.splice(randomIndex, 1);
+
+
+    console.log(color)
+}
